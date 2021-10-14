9f4ceea409c4e1adabd4027bc46c680f7d43aead
roodi <igorrud95@gmail.com>
2021-10-14 23:43:20 +0700

    passed 71-75 except 73

---
+++ JS/basic JS/074.js
@@ -0,0 +1,30 @@
+//Selecting from Many Options with Switch Statements
+
+function caseInSwitch(val) {
+    var answer = "";
+    // Only change code below this line
+  
+    switch(val) {
+      case 1:
+        answer = 'alpha'
+        console.log(answer);
+        break;
+      case 2:
+        answer = 'beta'
+        console.log(answer);
+        break;
+      case 3:
+        answer = 'gamma'
+        console.log(answer);
+        break;
+      case 4:
+        answer = 'delta'
+        console.log(answer);
+        break;
+    }
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  caseInSwitch(1);
