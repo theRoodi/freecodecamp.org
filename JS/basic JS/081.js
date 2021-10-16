c41419cb4b347828194e2dfd32dcd94815f817cf
roodi <igorrud95@gmail.com>
2021-10-16 23:01:17 +0700

    passed 81-85

---
+++ JS/basic JS/081.js
@@ -0,0 +1,11 @@
+// Build JavaScript Objects
+
+var myDog = {
+    // Only change code below this line
+      name: 'Bim',
+      legs: 4,
+      tails: 1,
+      friends: ['human', 'another dogs']
+    
+    // Only change code above this line
+    };
