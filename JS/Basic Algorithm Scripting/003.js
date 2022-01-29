079ced614d2c5441e2360ac2d4388049cd623163
roodi <igorrud95@gmail.com>
2022-01-29 22:25:55 +0700

    6-3/16

---
+++ JS/Basic Algorithm Scripting/003.js
@@ -0,0 +1,11 @@
+//Factorialize a Number
+
+function factorialize(num) {
+    let factorial = 1;
+    for(let i = 1; i <= num; i++){
+      factorial *= i;
+    }
+    return factorial;
+  }
+  
+  console.log(factorialize(0))
