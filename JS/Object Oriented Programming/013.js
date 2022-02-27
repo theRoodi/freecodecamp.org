c1a5f07f37ba163131275751fe46228d04af9b4a
roodi <igorrud95@gmail.com>
2022-02-27 23:09:44 +0700

    12-14

---
+++ JS/Object Oriented Programming/013.js
@@ -0,0 +1,18 @@
+//Change the Prototype to a New Object
+
+function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype = {
+    // Only change code below this line
+    numLegs : 4,
+    eat : function(){
+      console.log("nom nom nom");
+    },
+    describe : function(){
+      console.log("My name is " + this.name);
+    }
+  };
+  let dog = new Dog('dogy')
+  console.log(dog.eat())
