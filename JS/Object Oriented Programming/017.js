546d4a8283d84f9f4059e4b21f19e11b5ec75a9a
roodi <igorrud95@gmail.com>
2022-03-01 23:14:12 +0700

    17-20 and HTML 1-5

---
+++ JS/Object Oriented Programming/017.js
@@ -0,0 +1,28 @@
+//Use Inheritance So You Don't Repeat Yourself
+
+function Cat(name) {
+    this.name = name;
+  }
+  
+  Cat.prototype = {
+    constructor: Cat,
+    
+  };
+  
+  function Bear(name) {
+    this.name = name;
+  }
+  
+  Bear.prototype = {
+    constructor: Bear,
+    
+  };
+  
+  function Animal() { }
+  
+  Animal.prototype = {
+    constructor: Animal,
+    eat: function() {
+      console.log("nom nom nom");
+    }
+  };
---
+++ JS/Object Oriented Programming/019.js
@@ -0,0 +1,17 @@
+//Set the Child's Prototype to an Instance of the Parent
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+Dog.prototype = Object.create(Animal.prototype)// Only change code below this line
+
+
+let beagle = new Dog();
